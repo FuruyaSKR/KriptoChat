775438386aba13de29968b058bfea9180c5d2632
Victor Hugo Z. <victorhugo@offertech.com.br>
2025-05-26 22:17:16 -0300

    Migrates project to Maven and JavaFX
    Converts the existing NetBeans project to a Maven-based JavaFX project.
    
    This change introduces a `pom.xml` file to manage dependencies and build process, and restructures the source code to adhere to Maven conventions. It also adds necessary dependencies for JavaFX, JFoenix, and Ikonli.
    
    Removes the old NetBeans build system and related files.

---
+++ ChatExecutor/src/main/java/com/chat/sockets/ServidorSocket.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.chat.sockets;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author Furuya
+ */
+public class ServidorSocket {
+
+    public static void main(String[] args) {
+
+        ServerSocket servidor = null;
+
+        try {
+            System.out.println("Iniciando o Servidor...");
+            servidor = new ServerSocket(8888);
+            System.out.println("Server Open");
+
+            while (true) {
+                Socket cliente = servidor.accept();
+                new GerenciadorClientes(cliente);
+            }
+
+        } catch (IOException e) {
+
+            try {
+                if (servidor != null) {
+                    System.out.println("Server Closed");
+                    servidor.close();
+                }
+            } catch (IOException e1) {
+            }
+            System.err.println("Esta Porta está sendo ocupada, ou  servidor foi fechado!");
+            // e.printStackTrace();
+        }
+    }
+}
